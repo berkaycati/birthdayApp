f6022a371cb35519cee09fd250f4d695d943e46e
berkaycati <berkaycati@hotmail.com>
2022-08-19 02:24:29 +0300

    Initial Commit

---
+++ birthdayApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  birthdayApp
+//
+//  Created by Berkay on 19.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
